7104dbe92d33f54a2fc6953ecc5b6ceedbd0260b
NoelVahajylkka <t1vano00@students.oamk.fi>
2023-10-12 13:02:04 +0300

    1

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function showAlert() {
     alert('hähää!');
 }
+
+const playButton = document.getElementById("popup-button");
+        const audioElement = document.getElementById("audio-element");
+
+        // Add a click event listener to the button
+        playButton.addEventListener("click", function() {
+            if (audioElement.paused) {
+                // If the audio is paused, play it
+                audioElement.play();
+                playButton.innerHTML = "Pause Audio";
+            } else {
+                // If the audio is playing, pause it
+                audioElement.pause();
+                playButton.innerHTML = "Play Audio";
+            }
+        });
