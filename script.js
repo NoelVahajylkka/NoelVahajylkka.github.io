e3c5f1ca5f4f2089354c56cc39b65cd31f947d9a
NoelVahajylkka <t1vano00@students.oamk.fi>
2023-10-15 21:22:04 +0300

    a

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const audioElement = document.getElementById("audio-element");
+const hedelmat = [""]
 
 function showAlert() {
     audioElement.play();
     alert('hähää!');
+}
+
+function hedelma() {
+    fruits.push("Lemon");
 }
